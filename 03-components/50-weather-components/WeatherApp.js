2c52e9f831ec48e97475a6c432637455acbd3455
@borisevich.o <borisevich.olga94@gmail.com>
2025-07-13 23:28:55 +0300

    add 5 task for components

--- 03-components/50-weather-components/WeatherApp.js
+++ 03-components/50-weather-components/WeatherApp.js
@@ -1,51 +1,70 @@
 import { defineComponent } from 'vue'
 import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
+import WeatherCard from './WeatherCard.js'
 import './WeatherApp.css'
 
 export default defineComponent({
   name: 'WeatherApp',
+
+  components: {
+    WeatherCard,
+    WeatherConditionIcons,
+  },
+
+  setup() {
+    const weatherCards = getWeatherData()
+
+    function temperatureKelvinToCelsius(temp) {
+      return (temp - 273.15).toFixed(1)
+    }
+
+    function pressureСalculations(pressure) {
+      return (pressure * 0.75).toFixed(0)
+    }
+
+    function isNight(weatherCard) {
+      const toMinutes = (timeStr) => {
+        const [hours, minutes] = timeStr.split(':').map(Number)
+        return hours * 60 + minutes
+      }
+
+      const current = toMinutes(weatherCard.current.dt)
+      const sunrise = toMinutes(weatherCard.current.sunrise)
+      const sunset  = toMinutes(weatherCard.current.sunset)
+
+      return current < sunrise || current > sunset
+    }
+
+    return {
+      weatherCards,
+      temperatureKelvinToCelsius,
+      pressureСalculations,
+      WeatherConditionIcons,
+      isNight,
+    }
+  },
 
   template: `
     <div>
       <h1 class="title">Погода в Средиземье</h1>
 
       <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
-          <div class="weather-alert">
-            <span class="weather-alert__icon">⚠️</span>
-            <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
-          </div>
-          <div>
-            <h2 class="weather-card__name">
-              Гондор
-            </h2>
-            <div class="weather-card__time">
-              07:17
-            </div>
-          </div>
-          <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
-            <div class="weather-conditions__temp">15.0 °C</div>
-          </div>
-          <div class="weather-details">
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-              <div class="weather-details__item-value">754</div>
-            </div>
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
-            </div>
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
-            </div>
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
-            </div>
-          </div>
-        </li>
+        <WeatherCard
+          v-for="(card, index) in weatherCards"
+          :key="index"
+          :city="card.geographic_name"
+          :time="card.current.dt"
+          :temperature="temperatureKelvinToCelsius(card.current.temp)"
+          :pressure="pressureСalculations(card.current.pressure)"
+          :humidity="card.current.humidity"
+          :cloudiness="card.current.clouds"
+          :wind="card.current.wind_speed"
+          :weatherIcon="WeatherConditionIcons[card.current.weather.id]"
+          :conditionDescription="card.current.weather.description"
+          alertMessage="Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма."
+          :isNight="isNight(card)"
+          :alert="card.alert"
+        />
       </ul>
     </div>
   `,
